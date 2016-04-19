7799cc2ed550df266870c510e30e1b4e23df05a6
Kevin Martensson <kevinmartensson@gmail.com>
2016-04-19 15:40:01 +0200

    Add more tests

--- test.js
+++ test.js
@@ -2,12 +2,32 @@ import execa from 'execa';
 import imageSize from 'image-size';
 import test from 'ava';
 
-test('resize image', async t => {
+test('resize png image', async t => {
 	const stdout = (await execa('./cli.js', ['fixture.png', '--width', '150'], {encoding: null})).stdout;
 
 	t.deepEqual(imageSize(stdout), {
 		width: 150,
 		height: 99,
 		type: 'png'
+	});
+});
+
+test('resize jpg image', async t => {
+	const stdout = (await execa('./cli.js', ['fixture.jpg', '--width', '150'], {encoding: null})).stdout;
+
+	t.deepEqual(imageSize(stdout), {
+		width: 150,
+		height: 99,
+		type: 'jpg'
+	});
+});
+
+test('resize bmp image', async t => {
+	const stdout = (await execa('./cli.js', ['fixture.bmp', '--width', '150'], {encoding: null})).stdout;
+
+	t.deepEqual(imageSize(stdout), {
+		width: 150,
+		height: 99,
+		type: 'bmp'
 	});
 });
